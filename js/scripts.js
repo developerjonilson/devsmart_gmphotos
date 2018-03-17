3d85a31650b28ebe5f9e4610d4f8a80ebdfa8fd1
jonilsondeveloper <jonilsondeveloper@gmail.com>
2018-03-17 16:01:52 -0300

    Parceiros funcionando falta só colocar os parceiros reais

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:50,
+  center: true,
+  autoWidth:true,
+  autoplay:true,
+  autoplayTimeout:2000,
+  autoplayHoverPause:true,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:2,
+          nav:false
+      },
+      1000:{
+          items:4,
+          nav:false,
+          loop:true
+      }
+  }
+})
