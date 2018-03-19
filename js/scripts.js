fea11f321b4965b9a0e2d718b7cc7b7e316e1e1e
jonilsondeveloper <jonilsondeveloper@gmail.com>
2018-03-18 21:49:55 -0300

    só falta galeria

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,67 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+
+
+var owlCarousel = $('.carousel-marriage');
+$('#marriage1').on('shown.bs.modal', function (event) {
+  owlCarousel.owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    items: 4
+  });
+});
+$('#marriage1').on('hidden.bs.modal', function (event) {
+  owlCarousel.data('owlCarousel').destroy();
+  owlCarousel.find('.owl-stage-outer').children().unwrap();
+});
+$('.carousel-marriage').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    items: 4
+});
+
+// Start Gallery:
+
+// Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
